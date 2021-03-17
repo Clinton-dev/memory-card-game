d31e3ad8af4e29b715b94398c621165c67a645f3
Ryan Njoroge <keddelyronjoz@gmail.com>
2021-03-17 11:08:24 +0300

    fix: Fixed page to rerender after image click
    -Added clickedChar to the dependency array of useEffect so that the
    page rerenders on click of an image
    -Added handle score functionality
    -Changed react-scripts to 4.0.1 since it was giving me errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import ScoreBoard from "./component/ScoreBoard";
 import Card from "./component/Card";
 import './App.css';
+
+
+let clickedChar = [];
 
 function App() {
   const [score, setScore] = useState(0);
   const [bestscore, setBestScore] = useState(0);
   const [clicked, setclicked] = useState([]);
   const [charactersList, setCharactersList] = useState([]);
-  let clickedChar = [];
 
   function fetchData() {
     fetch("https://rickandmortyapi.com/api/character/[1,2,3,4,5,6,7,8]")
@@ -17,21 +19,36 @@ function App() {
   }
 
   
-  useEffect(() => { fetchData(); }, []);
+  useEffect(() => { 
+    // Only call the api when the characterList array is empty
+    if (charactersList.length === 0) {
+    fetchData(); 
+    }
+  }, [clickedChar]);
   
-  const characterComponent = charactersList.map(character => <Card key={character.id} name={character.name} image={character.image} clickfunct={handleClick} charId={character.id}/>);  
-  
-  function handleClick(idx) {
-   clickedChar.push(idx);
+  const characterComponent = charactersList.map(character => <Card key={character.id} name={character.name} image={character.image} clickfunct={() => handleClick(character.id)} charId={character.id}/>);  
+
+  // It's better to use ids to track which picture has been clicked.
+  function handleClick(id) {
+    if (clickedChar.includes(id)) {
+      handleScore('remove')
+    } else {
+      clickedChar.push(id);
+      handleScore();
+    }
+    console.log(clickedChar)
    shuffleAry();
-   handleScore();
   }
   
-  function handleScore() {
+  function handleScore(params) {
     // score to 0 if user clicked a picture that was already clicked 
     // add score if pic has not been clicked before
-    console.log(clickedChar);
-    console.log(charactersList);
+    // Remove 1 if a user clicks on a pic that has been clicked before.
+    if (params === 'remove') {
+      setScore(score - 1)
+    } else {
+      setScore(score + 1)
+    }
   }
 
   function shuffleAry() {
@@ -57,7 +74,7 @@ function App() {
       <ScoreBoard points={score} bScore={bestscore} />
       <br />
       <div className="row row-cols-3 row-cols-md-3 g-4">
-        { characterComponent } 
+        {characterComponent } 
       </div>
     </div>
   );
